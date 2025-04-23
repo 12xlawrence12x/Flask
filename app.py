5793d808269906df7ba5bf70d1367e1a6b59fe0b
12xlawrence12x <johnlawrence.rosaupan11@gmail.com>
2025-04-23 23:14:00 +0800

    third commit

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return "Hello from Flask on Heroku!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",500))
+    # app.run(debug=True)
+    app.run(host='0.0.0.0',port=port)
